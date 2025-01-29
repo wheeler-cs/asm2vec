061ee7d0f88e46cf2017a766e33052c0ac3b7ce1
Anna <wheelercs@protonmail.com>
2025-01-29 07:31:47 -0600

    Working on Setup Scripting
    Working on setting up scripts so that package can be installed using pip.
    
    Signed-off-by: Anna <wheelercs@protonmail.com>

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="asm2vec",
+    version="1.0",
+    description="An unofficial implementation of asm2vec as a standalone Python package.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/wheeler-cs/asm2vec",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "unittest",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+)
